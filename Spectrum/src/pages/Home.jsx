5238f9b31b5f847b69550db7ee88ff4ccfa2e9b6
박성준 <freestarjun@gmail.com>
2025-10-15 09:06:52 +0900

    메인페이지 제목style 적용

--- Spectrum/src/pages/Home.jsx
+++ Spectrum/src/pages/Home.jsx
@@ -5,6 +5,14 @@ export default function Home() {
 
         <div className = "Main-div">
           <img src = "Main-Logo.svg" className = "Main-Logo"></img>
+          <h1 className="Main-Title">
+            <span>보</span>
+            <span>여</span>
+            <span>줄</span>
+            <span>게 </span>
+            <span>스펙트럼 </span>
+            <span>!</span>
+          </h1>
         </div>
         
       </main>
